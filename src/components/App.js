667fffe667c868e8f898b5f29aca8a0db991a559
Cem <cemozturan@gmail.com>
2017-08-06 00:54:02 +0100

    Hooked up the first redux action & reducer to the app component!

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-		  <div className="title">
-			  Reminder Pro
-		  </div>
-		  <div className="form-inline">
-			  <div className="form-group">
-				  <input
-					  className="form-control"
-					  placeholder="I have to..."
-				  />
-				  <button type="button" className="btn btn-success">Add Reminder</button>
-			  </div>
-		  </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: ''
+		}
+		this.handleTextChange = this.handleTextChange.bind(this);
+		this.addReminder = this.addReminder.bind(this);
+	}
+
+	handleTextChange(evt) {
+		let text = evt.target.value;
+		this.setState(() => {
+			return {
+				text
+			}
+		});
+	}
+
+	addReminder() {
+		this.props.addReminder(this.state.text);
+	}
+
+ 	render() {
+    	return (
+      		<div className="App">
+				<div className="title">
+					Reminder Pro
+				</div>
+				<div className="form-inline">
+					<div className="form-group">
+						<input
+							className="form-control"
+							placeholder="I have to..."
+							value={this.state.text}
+							onChange={this.handleTextChange}
+						/>
+						<button
+							type="button"
+							className="btn btn-success"
+							onClick={this.addReminder}
+						>Add Reminder</button>
+					</div>
+				</div>
+			</div>
+    	);
+  	}
 }
 
-export default App;
+export default connect(null, { addReminder })(App);
