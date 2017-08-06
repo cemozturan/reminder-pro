ff4b9258c6b46dca7dc344c58993f711108ea111
Cem <cemozturan@gmail.com>
2017-08-06 18:53:20 +0100

    Using cookies to make the reminders persist ("sfcookies" package is used for this)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
 	const { text, dueDate } = action.payload;
@@ -11,12 +12,15 @@ const reminder = (action) => {
 
 const reminders = (state = [], action) => {
 	let reminders = null;
+	state = read_cookie('reminders');
 	switch(action.type) {
 		case ADD_REMINDER:
 			reminders = [...state, reminder(action)];
+			bake_cookie('reminders', reminders);
 			return reminders;
 		case DELETE_REMINDER:
 			reminders = state.filter((reminder) => reminder.id !== action.id);
+			bake_cookie('reminders', reminders);
 			return reminders;
 		default:
 			return state;
